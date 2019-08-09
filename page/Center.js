ab1165c2c23d4136457085d33aef5a0409a4df05
Zion0707 <zion19920707@gmail.com>
2019-08-09 16:38:00 +0800

    添加网络请求

--- page/Center.js
+++ page/Center.js
@@ -2,21 +2,57 @@ import React, { Component } from 'react';
 import {
     View, 
     Text,
-    Button
+    Button,
+    StyleSheet,
+    Alert
 } from 'react-native';
 
 import Styles from '../assets/js/Styles';
+import Utils from '../assets/js/Utils';
 
 export default class Center extends Component{
+    constructor(props){
+        super(props);
+        this.state={
+            jsonData:''
+        }
+    }
 
     componentWillUnmount(){
         console.log('卸载');
     }
+
+    componentDidMount(){
+        var _self = this;
+        //数据请求
+        fetch('https://facebook.github.io/react-native/movies.json')
+        .then((response) => response.json()) //
+        .then((res) => {
+            _self.setState({
+                jsonData: JSON.stringify(res)
+            });
+        })
+        .catch((error) => {
+            console.error(error);
+        });
+    }
+
     render(){
         return (
             <View>
                 <Text>Center</Text>
+                <View>
+                    <Text style={styles.h2}>数据</Text>
+                    <Text>{this.state.jsonData}</Text>
+                </View>
             </View>
         )
     }
 }
+
+const styles = StyleSheet.create({
+    h2:{
+        fontSize: Utils.pTd(30),
+        fontWeight: 'bold'
+    }
+});
